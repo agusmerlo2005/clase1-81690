5d3dfb4e787a7510979347f8d707343842e95475
Agustin Merlo <agusmerlo2005@icloud.com>
2025-10-27 19:08:15 -0300

    Primer Clase

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Boton from "./Boton/Boton"
+
+
+const App = () => {
+  return (
+<>
+<h1>Clase 1 de React</h1>
+    <h2>subtitulo</h2>
+    <Boton/>
+    <Boton/>
+    <Boton/>
+    <Boton/>
+    <Boton/>
+    <Boton/>
+    <Boton/>
+</>
+
+  )
+}
+
+export default App
